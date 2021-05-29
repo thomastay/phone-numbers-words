0e8501503a51677093f5b63052fd22ad51a15c1e
ttay <ttay@umich.edu>
2021-05-29 15:04:44 -0700

    add docs

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,5 +1,24 @@
-#![allow(missing_docs)] // don't publish a crate with this!
-#![allow(dead_code)] // don't publish a crate with this!
+//! A Rust translation of [Norvig's Lisp as an alternative to Java](https://norvig.com/java-lisp.html)
+//! This is a fun little exercise that I use when learning a new language.
+//! I've done this previous excursion in Zig, Nim and Clojure and Python [Github link](https://github.com/thomastay/phone-numbers-words)
+//! The gist of this problem is as such: Given some mapping of letters to digits,
+//! we want to "translate" a phone number into a series of words that encode the phone number
+//! The words will be given to us via some dictionary.
+//! [The full instructions are here:](https://github.com/thomastay/phone-numbers-words/blob/master/resources/test_instructions.txt)
+//!
+//! For instance, given the mapping below, and a dictionary of words:
+//! MAPPING:
+//!       E | J N Q | R W X | D S Y | F T | A M | C I V | B K U | L O P | G H Z
+//!       e | j n q | r w x | d s y | f t | a m | c i v | b k u | l o p | g h z
+//!       0 |   1   |   2   |   3   |  4  |  5  |   6   |   7   |   8   |   9
+//! DICT: {hell, hello, o, world, row, oy}
+//! we might encode the number 908-882-8283 in 4 different ways:
+//!    1. hello world
+//!    2. hell o world
+//!    3. hello row oy
+//!    4. hell o row oy
+
+#![deny(missing_docs)]
 #![deny(rust_2018_idioms)]
 #![deny(clippy::too_many_arguments)]
 #![deny(clippy::complexity)]
@@ -10,38 +29,31 @@
 
 use std::{
     collections::HashMap,
+    env,
     error::Error,
     fs::File,
     io::{BufRead, BufReader},
     path::Path,
+    process::exit,
 };
 
 const DIGIT_MAP: [u8; 26] = [
     53, 55, 54, 51, 48, 52, 57, 57, 54, 49, 55, 56, 53, 49, 56, 56, 49, 50, 51, 52, 55, 54, 50, 50,
     51, 57,
-]; // auto gen-ed for now. See Zig code for how it is autogen'd
+]; // auto gen-ed for now. See bottom of file for how it is autogen'd
 
 type WordsDictionary = HashMap<String, Vec<String>>;
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let dict = {
-        let dict_filename = "../resources/dictionary_small.txt";
-        let dict_path = Path::new(dict_filename);
-        let dict_file = File::open(dict_path)?;
-        let mut dict: HashMap<String, Vec<String>> = HashMap::new();
-        for line in BufReader::new(dict_file).lines() {
-            let line = line?;
-            let digits = word_to_num(&line);
-            if let Some(v) = dict.get_mut(&digits) {
-                v.push(line)
-            } else {
-                dict.insert(digits, vec![line]);
-            }
-        }
-        dict
-    };
-    // println!("{:?}", dict);
-    let phone_num_filename = "../resources/input_small.txt";
+    let args = env::args().skip(1).collect::<Vec<_>>();
+    if args.len() != 2 {
+        eprintln!("Usage: ./phone_number_words <dict filename> <input filename>");
+        exit(1); // Note: was annoying that String does not implement the error trait.
+    }
+    let dict_filename = &args[0];
+    let phone_num_filename = &args[1];
+
+    let dict = read_dict_from_file(dict_filename)?;
     let phone_num_path = Path::new(phone_num_filename);
     let phone_num_file = File::open(phone_num_path)?;
     for phone_num in BufReader::new(phone_num_file).lines() {
@@ -53,6 +65,24 @@ fn main() -> Result<(), Box<dyn Error>> {
     Ok(())
 }
 
+fn read_dict_from_file(dict_filename: &str) -> Result<WordsDictionary, Box<dyn Error>> {
+    let dict_path = Path::new(dict_filename);
+    let dict_file = File::open(dict_path)?;
+    let mut dict: HashMap<String, Vec<String>> = HashMap::new();
+    for line in BufReader::new(dict_file).lines() {
+        let line = line?;
+        let digits = word_to_num(&line);
+        if let Some(v) = dict.get_mut(&digits) {
+            v.push(line)
+        } else {
+            dict.insert(digits, vec![line]);
+        }
+    }
+    Ok(dict)
+}
+
+/// For a given phone number and a dictionary mapping digits to phone numbers,
+/// prints the phone number and all combinations of words that might encode the phone number.
 fn print_translation(phone_number: &str, dict: &WordsDictionary) {
     // helper function that does the real work recursively
     fn helper(
@@ -77,6 +107,7 @@ fn print_translation(phone_number: &str, dict: &WordsDictionary) {
         // e.g. digits: 5  6  2  4  8  2
         // start: 3              ^
         // end: [4, 5, 6]
+        // TODO make this slicing better, probably.
         for end in (start + 1)..=digits.len() {
             let key = &digits[start..end];
             // println!("Searching for key {}", key);
@@ -84,10 +115,13 @@ fn print_translation(phone_number: &str, dict: &WordsDictionary) {
                 found_word = true;
                 for word in words_mapped_to_digit {
                     // Recurse. Push onto word_list before recursion, and pop after.
+                    // TODO remove this clone somehow
                     word_list.push(word.to_string());
                     helper(word_list, end, phone_number, digits, dict);
-                    let x = word_list.pop(); // ignore the popped value
-                    debug_assert!(x.is_some());
+                    let last = word_list.pop();
+                    // Sanity check: upon reaching this point, the last elt of the vec should be the one
+                    // we just pushed on.
+                    debug_assert_eq!(last, Some(word.to_string()));
                 }
             }
         }
@@ -96,8 +130,10 @@ fn print_translation(phone_number: &str, dict: &WordsDictionary) {
 
             word_list.push(single_digit);
             helper(word_list, start + 1, phone_number, digits, dict);
-            let x = word_list.pop(); // ignore the popped value
-            debug_assert!(x.is_some());
+            let last = word_list.pop();
+            // Sanity check: upon reaching this point, the last elt of the vec should be the one
+            // we just pushed on.
+            debug_assert_eq!(last, Some((digits.as_bytes()[start] as char).to_string()));
         }
     }
 
@@ -106,11 +142,11 @@ fn print_translation(phone_number: &str, dict: &WordsDictionary) {
     helper(&mut word_list, 0, phone_number, &digits, dict);
 }
 
-/// Name says it all.
 fn is_empty_or_last_elt_is_not_single_digit(v: &[String]) -> bool {
     v.last().map_or(true, |s| s.len() != 1)
 }
 
+/// e.g. helloworld --> 9088828283
 fn word_to_num(word: &str) -> String {
     word.bytes()
         .filter(u8::is_ascii_alphabetic)
@@ -118,6 +154,7 @@ fn word_to_num(word: &str) -> String {
         .collect()
 }
 
+/// e.g. 908-882/8283 --> 9088828283
 fn only_digits(word: &str) -> String {
     word.chars().filter(char::is_ascii_digit).collect()
 }
